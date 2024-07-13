0982a01a214ffd79a7826d804f58c0413314bf6b
ADmad <admad.coder@gmail.com>
2024-07-13 19:00:55 +0530

    Fix error on PHP 8.4
    PHP 8.4 adds a new interface method DatetimeImmutable::createFromTimestamp().

--- src/Chronos.php
+++ src/Chronos.php
@@ -736,13 +736,23 @@ class Chronos extends DateTimeImmutable implements Stringable
     /**
      * Create an instance from a timestamp
      *
-     * @param int $timestamp The timestamp to create an instance from.
+     * @param float|int $timestamp The timestamp to create an instance from.
      * @param \DateTimeZone|string|null $timezone The DateTimeZone object or timezone name the new instance should use.
      * @return static
      */
-    public static function createFromTimestamp(int $timestamp, DateTimeZone|string|null $timezone = null): static
+    public static function createFromTimestamp(float|int $timestamp, DateTimeZone|string|null $timezone = null): static
     {
-        return static::now($timezone)->setTimestamp($timestamp);
+        if (PHP_VERSION_ID >= 80400 && $timezone === null) {
+            return parent::createFromTimestamp($timestamp);
+        }
+
+        $instance = static::now($timezone);
+
+        if (is_int($timestamp)) {
+            return $instance->setTimestamp($timestamp);
+        }
+
+        return $instance->modify('@' . $timestamp);
     }
 
     /**
